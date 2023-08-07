3fb77c1ac65325b5aba62e61e0342d37a9873903
Akarm Adjab <akarmadjab@Akarms-MacBook-Pro.local>
2023-08-07 15:55:06 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
@@ -8,15 +8,5 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    {/* <StarRating maxRating={5} defaultRating={3} />
-    <StarRating
-      maxRating={5}
-      color="red"
-      size={24}
-      className="test"
-      messages={["Terrible", "Bad", "Okay", "Good", "Amzaing"]}
-    />
-
-    <Test /> */}
   </React.StrictMode>
 );
